aab10c998bc57b49cd62418469c89f3410034887
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-08-14 06:26:06 +0700

    Add spinner for loading Message List

---
+++ src/components/MyLoading/index.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from "flowbite-react";
+
+const MyLoading = () => {
+  return (
+    <div>
+      <Spinner /> Loading...
+    </div>
+  );
+};
+
+export default MyLoading;
--- src/pages/Message/index.tsx
+++ src/pages/Message/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getChatList } from "../../api/message";
 import { Avatar } from "flowbite-react";
 import { useNavigate } from "react-router-dom";
+import MyLoading from "../../components/MyLoading";
 
 interface ChatListProfile {
   userId: number;
@@ -14,22 +15,27 @@ interface ChatListProfile {
 
 const ChatList = () => {
   const [contactList, setContactList] = useState<ChatListProfile[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     getChatList()
       .then((response) => {
-        setContactList(response.data);
+        console.log(response.data);
+        setContactList(response.data ?? []);
+        setIsLoading(false);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
-  return (
-    <div>
-      {contactList &&
-        contactList.map((contact) => (
+  const ContactList: React.FC = () => {
+    return isLoading ? (
+      <MyLoading />
+    ) : (
+      <div>
+        {contactList.map((contact) => (
           <div
             key={contact.userId}
             className="flex w-full p-3 gap-3 active:scale-95 active:rounded-full hover:bg-slate-200 dark:hover:bg-slate-700 transition-all myPointer"
@@ -42,6 +48,13 @@ const ChatList = () => {
             </div>
           </div>
         ))}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <ContactList />
     </div>
   );
 };
